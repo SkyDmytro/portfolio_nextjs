55a08175c5a1f7a5cb4a410158c19d4581a0e085
Dima <skydmytro@gmail.com>
2024-10-09 14:39:14 +0200

    feat: add tempates for future sections

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "./fonts/SourceSansPro-Black.otf";
+import "./fonts/SourceSansPro-BlackIt.otf";
+import "./fonts/SourceSansPro-Bold.otf";
+import "./fonts/SourceSansPro-BoldIt.otf";
+import "./fonts/SourceSansPro-ExtraLight.otf";
+import "./fonts/SourceSansPro-ExtraLightIt.otf";
+import "./fonts/SourceSansPro-Regular.otf";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
