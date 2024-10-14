d87bbb6ee273fd16d88fe81e3925e378b95d5abe
Dima <skydmytro@gmail.com>
2024-10-14 18:22:31 +0200

    feat: add scroll to top button

---
+++ src/components/ScrollButton.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import arrow from "../assets/up-arrow-button-svgrepo-com.svg";
+export const ScrollButton = () => {
+  const handleScrollOnTop = () => {
+    window.scrollTo(0, 0);
+  };
+  return (
+    <div
+      onClick={handleScrollOnTop}
+      className="fixed sm:bottom-12 sm:right-5 md:bottom-5 md:right-5 h-8  w-8 cursor-pointer hover:bg-primaryFont hover:text-blue transition duration-500 bg-lightBlue bg-opacity-50 rounded-full "
+    >
+      <img src={arrow} alt="arrow" />
+    </div>
+  );
+};
