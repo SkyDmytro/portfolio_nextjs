1afae319d915606364bae32f8b388e8a63cb6d50
Dima <skydmytro@gmail.com>
2024-10-17 17:30:05 +0200

    feat: add new pages templates

---
+++ src/pages/TypingTestProjectPage.tsx
@@ -0,0 +1,12 @@
+import { ProjectPageLayout } from "../components/ProjectPageLayout";
+
+export const TypingTestProjectPage = () => {
+  return (
+    <ProjectPageLayout
+      projectUrl="https://skytype.vercel.app/"
+      githubUrl="https://github.com/SkyDmytro/typing_test"
+      projectDescription="Lorem"
+      projectName="Typing Speed Training Web Application"
+    />
+  );
+};
