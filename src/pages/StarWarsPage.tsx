7e1ca42ee4c55483f43f4699be5519069e8d9ffe
Dima <skydmytro@gmail.com>
2024-10-21 18:33:18 +0200

    hotfix: change the package to Seo tags

--- src/pages/StarWarsPage.tsx
+++ src/pages/StarWarsPage.tsx
@@ -1,51 +1,17 @@
 import { starWarsProject } from "../helpers/constants";
 import { ProjectPageLayout } from "../components/ProjectPageLayout/ProjectPageLayout";
-import { Helmet } from "react-helmet";
+import { Seo } from "@/components/Seo/Seo";
 
 export const StarWarsPage = () => {
   return (
     <>
-      <Helmet>
-        <title>Star Wars Data Base</title>
-        <meta
-          name="description"
-          content="Star Wars Data Base Showcase by Skydan Dmytro"
-        />
-
-        <meta
-          property="og:url"
-          content="https://star-wars-database-tt.netlify.app/"
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Star Wars Data Base" />
-        <meta
-          property="og:description"
-          content="Star Wars Data Base Showcase by Skydan Dmytro"
-        />
-        <meta
-          property="og:image"
-          content="https://ogcdn.net/2c2c6737-47d4-4459-9969-e711eb48394c/v1/Star%20Wars%20Data%20Base%20Showcase/Skydan%20Dmytro/Star%20Wars%20Data%20Base/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2Fa028500a-45c7-499a-9bda-67dd2f5c61bb.png%3Ftoken%3D71ftTwmBXcraArlRCAOQXAVZHnHEO1sCIwDxfurmbfA%26height%3D625%26width%3D1200%26expires%3D33265525242/og.png"
-        />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          property="twitter:domain"
-          content="star-wars-database-tt.netlify.app"
-        />
-        <meta
-          property="twitter:url"
-          content="https://star-wars-database-tt.netlify.app/"
-        />
-        <meta name="twitter:title" content="Star Wars Data Base" />
-        <meta
-          name="twitter:description"
-          content="Star Wars Data Base Showcase by Skydan Dmytro"
-        />
-        <meta
-          name="twitter:image"
-          content="https://ogcdn.net/2c2c6737-47d4-4459-9969-e711eb48394c/v1/Star%20Wars%20Data%20Base%20Showcase/Skydan%20Dmytro/Star%20Wars%20Data%20Base/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2Fa028500a-45c7-499a-9bda-67dd2f5c61bb.png%3Ftoken%3D71ftTwmBXcraArlRCAOQXAVZHnHEO1sCIwDxfurmbfA%26height%3D625%26width%3D1200%26expires%3D33265525242/og.png"
-        />
-      </Helmet>
+      <Seo
+        title="Star Wars Data Base"
+        description="Star Wars Data Base Showcase by Skydan Dmytro"
+        url="https://star-wars-database-tt.netlify.app/"
+        image="https://ogcdn.net/2c2c6737-47d4-4459-9969-e711eb48394c/v1/Star%20Wars%20Data%20Base%20Showcase/Skydan%20Dmytro/Star%20Wars%20Data%20Base/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2Fa028500a-45c7-499a-9bda-67dd2f5c61bb.png%3Ftoken%3D71ftTwmBXcraArlRCAOQXAVZHnHEO1sCIwDxfurmbfA%26height%3D625%26width%3D1200%26expires%3D33265525242/og.png"
+        imageAlt="Star Wars Data Base image preview"
+      />
       <ProjectPageLayout project={starWarsProject} />;
     </>
   );
--- src/pages/TypingTestProjectPage.tsx
+++ src/pages/TypingTestProjectPage.tsx
@@ -1,43 +1,18 @@
+import { Seo } from "@/components/Seo/Seo";
 import { ProjectPageLayout } from "../components/ProjectPageLayout/ProjectPageLayout";
 import { TypingTestProject } from "../helpers/constants";
-import { Helmet } from "react-helmet";
 
 export const TypingTestProjectPage = () => {
   const project = TypingTestProject;
   return (
     <>
-      <Helmet>
-        <title>Typing Test Project</title>
-        <meta
-          name="description"
-          content="Typing Test Project Showcase by Skydan Dmytro"
-        />
-
-        <meta property="og:url" content="https://skydmytro.vercel.app/" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Typing Test Project" />
-        <meta
-          property="og:description"
-          content="Typing Test Project Showcase by Skydan Dmytro"
-        />
-        <meta
-          property="og:image"
-          content="https://ogcdn.net/2c2c6737-47d4-4459-9969-e711eb48394c/v1/Typing%20Test%20Showcase/Skydan%20Dmytro/Typing%20Test%20Projcet/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2Fa028500a-45c7-499a-9bda-67dd2f5c61bb.png%3Ftoken%3D71ftTwmBXcraArlRCAOQXAVZHnHEO1sCIwDxfurmbfA%26height%3D625%26width%3D1200%26expires%3D33265525242/og.png"
-        />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="skydmytro.vercel.app" />
-        <meta property="twitter:url" content="https://skydmytro.vercel.app/" />
-        <meta name="twitter:title" content="Typing Test Project" />
-        <meta
-          name="twitter:description"
-          content="Typing Test Project Showcase by Skydan Dmytro"
-        />
-        <meta
-          name="twitter:image"
-          content="https://ogcdn.net/2c2c6737-47d4-4459-9969-e711eb48394c/v1/Typing%20Test%20Showcase/Skydan%20Dmytro/Typing%20Test%20Projcet/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2Fa028500a-45c7-499a-9bda-67dd2f5c61bb.png%3Ftoken%3D71ftTwmBXcraArlRCAOQXAVZHnHEO1sCIwDxfurmbfA%26height%3D625%26width%3D1200%26expires%3D33265525242/og.png"
-        />
-      </Helmet>
+      <Seo
+        imageAlt="Typing Test image preview"
+        title="Typing Test Project"
+        description="Typing Test Project Showcase by Skydan Dmytro"
+        url="https://skydmytro.vercel.app/"
+        image="https://ogcdn.net/2c2c6737-47d4-4459-9969-e711eb48394c/v1/Typing%20Test%20Showcase/Skydan%20Dmytro/Typing%20Test%20Projcet/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2Fa028500a-45c7-499a-9bda-67dd2f5c61bb.png%3Ftoken%3D71ftTwmBXcraArlRCAOQXAVZHnHEO1sCIwDxfurmbfA%26height%3D625%26width%3D1200%26expires%3D33265525242/og.png"
+      />
       <ProjectPageLayout project={project} />
     </>
   );
