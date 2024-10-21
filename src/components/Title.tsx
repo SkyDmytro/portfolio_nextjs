1db9b7985aa45ba0720181ce7ad80e8b7449382f
Dima <skydmytro@gmail.com>
2024-10-21 10:55:38 +0200

    feat: add animation to title of the main page

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,13 +1,16 @@
 import { ReactElement } from "react";
+import BlurFade from "./ui/blur-fade";
 
 export const Title = (): ReactElement => {
   return (
     <div className="text-center">
-      <h1 className="text-[62px] font-bold mb-4">Hey, I'm Skydan Dmytro</h1>
-      <p className="text-secondaryFont mx-auto w-[80%] text-[22px] leading-[26px] ">
-        A result-oriented web developer focused on creating and optimizing
-        websites and web applications.
-      </p>
+      <BlurFade delay={0.25} inView>
+        <h1 className="text-[62px] font-bold mb-4">Hey, I'm Skydan Dmytro</h1>
+        <p className="text-secondaryFont mx-auto w-[80%] text-[22px] leading-[26px] ">
+          A result-oriented web developer focused on creating and optimizing
+          websites and web applications.
+        </p>
+      </BlurFade>
     </div>
   );
 };
