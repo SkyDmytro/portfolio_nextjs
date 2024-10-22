0436d04c8bb97db5cf8746e8f57785310fa818c1
Dima <skydmytro@gmail.com>
2024-10-22 18:29:22 +0200

    hotfix: test commit seo tags

--- src/components/Seo/Seo.tsx
+++ src/components/Seo/Seo.tsx
@@ -11,7 +11,7 @@ interface SeoProps {
 export const Seo = ({ title, description, url, image, imageAlt }: SeoProps) => {
   return (
     <Helmet>
-      <title>{title} | MySiteName</title>
+      <title>{title} </title>
       <link rel="canonical" href={url} />
       <meta name="description" content={description} />
       <meta name="robots" content="index,follow" />
@@ -25,7 +25,7 @@ export const Seo = ({ title, description, url, image, imageAlt }: SeoProps) => {
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       <meta property="og:locale" content="en_US" />
-      <meta property="og:site_name" content="MySiteName" />
+      <meta property="og:site_name" content="Portfolio" />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
--- src/pages/MobileChatPage.tsx
+++ src/pages/MobileChatPage.tsx
@@ -1,4 +1,3 @@
-import { Seo } from "@/components/Seo/Seo";
 import { ProjectPageLayout } from "../components/ProjectPageLayout/ProjectPageLayout";
 import { mobileChatProject } from "../helpers/constants";
 import { Helmet } from "react-helmet-async";
@@ -31,13 +30,13 @@ export const MobileChatPage = () => {
         <meta property="og:locale" content="en_US" />
         <meta property="og:site_name" content="Skydan Dmytro" />
       </Helmet>
-      <Seo
+      {/* <Seo
         title="Mobile Chat Project"
         description="Mobile Chat Project Showcase"
         image="https://ogcdn.net/2c2c6737-47d4-4459-9969-e711eb48394c/v1/Typing%20Test%20Showcase/Skydan%20Dmytro/Typing%20Test%20Projcet/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2Fa68be212-d9d4-4326-98f7-37661db56ba1.png%3Ftoken%3DyPVRqt9JEbohyfm0V8cE55StrYN0j-g7JcRWyvHOAFs%26height%3D625%26width%3D1200%26expires%3D33265525242/og.png"
         imageAlt="Mobile chat image preview"
         url="https://skydmytro.vercel.app/"
-      />
+      /> */}
       <ProjectPageLayout project={mobileChatProject} />;
     </>
   );
