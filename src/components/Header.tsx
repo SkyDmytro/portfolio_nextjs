5e992e9cc80e01497aa7a61d115cc0064af5b83b
Dima <skydmytro@gmail.com>
2024-10-24 17:36:32 +0200

    style(header): fix header on top

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { HeaderNavigarionMenu } from "./HeaderNavigarionMenu";
 
 export const Header = (): ReactElement => {
   return (
-    <header className="bg-blue py-4 absolute w-full">
+    <header className="bg-blue py-4 w-full fixed z-10">
       <div className="container flex items-center justify-between font-bold">
         <span>Skydan Dmytro</span>
         <HeaderNavigarionMenu />
