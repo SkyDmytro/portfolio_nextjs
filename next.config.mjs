def6224fa678494456f566f3b5fa62859de93022
Dima <skydmytro@gmail.com>
2024-11-10 13:50:28 +0100

    build: add nextjs

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  distDir: './dist', // Chanoges the build output directory to `./dist/`.
+  reactStrictMode: true,
+};
+
+export default nextConfig;
