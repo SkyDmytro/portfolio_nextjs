f13c546b46fa74399380307b9f4fed5ff29321bd
Dima <skydmytro@gmail.com>
2024-11-11 15:57:29 +0100

    refactor: small changes

--- src/pages/StarWarsPage.tsx
+++ src/pages/StarWarsPage.tsx
@@ -1,17 +1,9 @@
-import { Seo } from '@/components/Seo/Seo';
-
 import { ProjectPageLayout } from '../components/ProjectPageLayout/ProjectPageLayout';
 import { starWarsProject } from '../helpers/constants';
 
 export const StarWarsPage = () => {
   return (
     <>
-      {' '}
-      <Seo
-        title="Star Wars Data Base"
-        description="Star Wars Data Base Showcase by Skydan Dmytro"
-        url="https://skydmytro.vercel.app/projects/starWars"
-      />
       <ProjectPageLayout project={starWarsProject} />;
     </>
   );
--- src/pages/TypingTestProjectPage.tsx
+++ src/pages/TypingTestProjectPage.tsx
@@ -1,5 +1,3 @@
-import { Seo } from '@/components/Seo/Seo';
-
 import { ProjectPageLayout } from '../components/ProjectPageLayout/ProjectPageLayout';
 import { TypingTestProject } from '../helpers/constants';
 
@@ -7,11 +5,6 @@ export const TypingTestProjectPage = () => {
   const project = TypingTestProject;
   return (
     <>
-      <Seo
-        title="Typing Test Project"
-        description="Typing Test Project Showcase by Skydan Dmytro"
-        url="https://skydmytro.vercel.app/projects/typingTest"
-      />
       <ProjectPageLayout project={project} />
     </>
   );
