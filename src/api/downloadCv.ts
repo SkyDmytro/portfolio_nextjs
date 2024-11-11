4efdf729a8e926b39679252b5d9577fea7bcde93
Dima <skydmytro@gmail.com>
2024-11-11 15:56:44 +0100

    refactor: delete unused files

--- src/api/downloadCv.ts
+++
@@ -1,8 +0,0 @@
-import { API_URL } from '../config';
-
-export const downloadCv = async () => {
-  const url = API_URL + '/download-cv';
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data);
-};
