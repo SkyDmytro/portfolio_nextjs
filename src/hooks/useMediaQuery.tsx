a8c394c89ec1366e8b41af6988e8e477ca2ae678
Dima <skydmytro@gmail.com>
2024-11-11 15:59:57 +0100

    feat: add throttle function to window resize

--- src/hooks/useMediaQuery.tsx
+++ src/hooks/useMediaQuery.tsx
@@ -4,13 +4,24 @@ const useMediaQuery = (width: number) => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
+    let timeoutId: NodeJS.Timeout | null = null;
+
     const handleResize = () => {
-      setIsMobile(window.innerWidth <= width);
+      if (timeoutId) return;
+      timeoutId = setTimeout(() => {
+        setIsMobile(window.innerWidth <= width);
+        timeoutId = null;
+      }, 500);
     };
 
-    handleResize(); // Set initial value
+    handleResize();
     window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+      window.removeEventListener('resize', handleResize);
+    };
   }, [width]);
 
   return isMobile;
