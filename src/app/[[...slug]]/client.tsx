c19ba93b453d23b5b706160cabd397518b0fe440
Dima <skydmytro@gmail.com>
2024-11-11 15:58:42 +0100

    feat: add next structure

--- src/app/[[...slug]]/client.tsx
+++ src/app/[[...slug]]/client.tsx
@@ -3,7 +3,12 @@
 import dynamic from 'next/dynamic';
 import React from 'react';
 
-const App = dynamic(() => import('../../App'), { ssr: false });
+import LoadingComponent from '../loading';
+
+const App = dynamic(() => import('../../App'), {
+  ssr: false,
+  loading: () => <LoadingComponent />,
+});
 
 export function ClientOnly() {
   return <App />;
--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -1,7 +1,12 @@
 import { ClientOnly } from './client';
 
 export function generateStaticParams() {
-  return [{ slug: [''] }, { slug: ['typingTest'] }, { slug: ['starWars'] }];
+  return [
+    { slug: [''] },
+    { slug: ['typingTest'] },
+    { slug: ['starWars'] },
+    { slug: ['chat'] },
+  ];
 }
 
 export default function Page() {
