be5477f6b528e029efbb18b8052fb448a4e13986
Dima <skydmytro@gmail.com>
2024-11-12 15:53:01 +0100

    hotfix: migrate to servers components by default

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,34 +1,8 @@
-'use client';
-
-import { Skeleton } from '../components/ui/skeleton';
-
 export default function Loading() {
   return (
-    <div className="min-h-screen bg-background">
-      <nav className="flex items-center justify-between p-4">
-        <Skeleton className="h-6 w-32" />
-        <div className="flex gap-6">
-          {[...Array(4)].map((_, i) => (
-            <Skeleton key={i} className="h-6 w-16" />
-          ))}
-        </div>
-      </nav>
-
-      <main className="container mx-auto flex min-h-[80vh] flex-col items-center justify-center px-4 text-center">
-        <div className="fixed left-6 top-1/2 -translate-y-1/2 space-y-4">
-          {[...Array(2)].map((_, i) => (
-            <Skeleton key={i} className="h-8 w-8 rounded-full" />
-          ))}
-        </div>
-
-        <div className="space-y-6">
-          <Skeleton className="mx-auto h-12 w-96 max-w-full" />
-          <Skeleton className="mx-auto h-16 w-[600px] max-w-full" />
-        </div>
-      </main>
-
-      <div className="fixed bottom-6 right-6">
-        <Skeleton className="h-10 w-10 rounded-full" />
+    <div className="min-h-screen flex bg-darkBlue justify-center items-center">
+      <div className="flex items-center justify-center h-full">
+        <div className="animate-spin rounded-full h-20 w-20 border-b-4 border-lightBlue"></div>
       </div>
     </div>
   );
---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+import App from '../App';
+
+const page = async () => {
+  return <App />;
+};
+export default page;
