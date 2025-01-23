698625acb26ef743bde1eb5d882952ec0ae584b3
Dima <skydmytro@gmail.com>
2025-01-23 14:52:24 +0100

    hotfix: other small changes

--- src/components/ProjectsPageComponent/ui/ProjectDescriptionLayout.tsx
+++ src/components/ProjectsPageComponent/ui/ProjectDescriptionLayout.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { motion } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
 import { ReactNode } from 'react';
@@ -24,15 +27,25 @@ export const ProjectDescriptionLayout = ({
       <span className="font-bold">{title}</span>
       <TechStack techs={techStack} />
       <div className="max-w-2xl">{children}</div>
-      <div className="hover-button-animation rounded-xl w-fit  ">
+      <motion.div
+        className="rounded-xl w-fit  text-white shadow-lg"
+        whileHover={{ scale: 1.05, backgroundColor: '#336D8B' }}
+        whileTap={{ scale: 0.95 }}
+        transition={{ type: 'spring', stiffness: 300, damping: 20 }}
+      >
         <Link
           href={projectUrl}
-          className="opacity-100 flex items-center gap-2 rounded-xl"
+          className="opacity-100 flex items-center gap-2 rounded-xl p-3"
         >
           <span className="opacity-100">Project Showcase</span>
-          <Image src={redirect} alt="redirect" className="h-4 w-4" />
+          <motion.div
+            whileHover={{ x: 5 }}
+            transition={{ type: 'spring', stiffness: 300 }}
+          >
+            <Image src={redirect} alt="redirect" className="h-4 w-4" />
+          </motion.div>
         </Link>
-      </div>
+      </motion.div>
     </div>
   );
 };
